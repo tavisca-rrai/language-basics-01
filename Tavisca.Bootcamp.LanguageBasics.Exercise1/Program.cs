5f5d88a6f2d94cb5ca26afa2103536d239977afc
tavisca-rrai <rrai@tavisca.in>
2019-07-05 17:05:39 +0530

    trail the space and modify the program as per your instruction

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -23,64 +23,48 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
-
             int loc_mul = equation.IndexOf("*");
             int loc_equal = equation.IndexOf("=");
+            string[] temp = equation.Split('*', '=');
+            //string temp_A = equation.Substring(0, loc_mul);
+            //string temp_B = equation.Substring(loc_mul + 1, loc_equal - loc_mul - 1);
+            //string temp_C = equation.Substring(loc_equal + 1);
 
-            string temp_A = equation.Substring(0, loc_mul);
-            string temp_B = equation.Substring(loc_mul + 1, loc_equal - loc_mul - 1);
-            string temp_C = equation.Substring(loc_equal + 1);
-
-
-            if (temp_A.Contains("?"))
+            if (temp[0].Contains("?"))
             {
-                string correct_ans = decimal.Divide(Int32.Parse(temp_C), Int32.Parse(temp_B)).ToString();
-
-
-                int loc_ques = temp_A.IndexOf("?");
-
-                if (correct_ans.Substring(0, loc_ques).Equals(temp_A.Substring(0, loc_ques)) && correct_ans.Substring(loc_ques + 1).Equals(temp_A.Substring(loc_ques + 1)))
+                string correct_ans = decimal.Divide(Int32.Parse(temp[2]), Int32.Parse(temp[1])).ToString();
+                int loc_ques = temp[0].IndexOf("?");
+                if (correct_ans.Substring(0, loc_ques).Equals(temp[0].Substring(0, loc_ques)) && correct_ans.Substring(loc_ques + 1).Equals(temp[0].Substring(loc_ques + 1)))
                 {
-
                     return (int)Char.GetNumericValue(correct_ans[loc_ques]);
                 }
                 else
                 { return -1; }
 
             }
-            else if (temp_B.Contains("?"))
+            else if (temp[1].Contains("?"))
             {
-
-                string correct_ans = decimal.Divide(Int32.Parse(temp_C), Int32.Parse(temp_A)).ToString();
-                Console.WriteLine(correct_ans);
-                int loc_ques = temp_B.IndexOf("?");
-
-                if (correct_ans.Substring(0, loc_ques).Equals(temp_B.Substring(0, loc_ques)) && correct_ans.Substring(loc_ques + 1).Equals(temp_B.Substring(loc_ques + 1)))
+                string correct_ans = decimal.Divide(Int32.Parse(temp[2]), Int32.Parse(temp[0])).ToString();
+                
+                int loc_ques = temp[1].IndexOf("?");
+                if (correct_ans.Substring(0, loc_ques).Equals(temp[1].Substring(0, loc_ques)) && correct_ans.Substring(loc_ques + 1).Equals(temp[1].Substring(loc_ques + 1)))
                 {
                     return (int)Char.GetNumericValue(correct_ans[loc_ques]);
                 }
                 else
                 { return -1; }
-
             }
             else
             {
-
-                string correct_ans = (Int32.Parse(temp_A) * Int32.Parse(temp_B)).ToString();
-
-                int loc_ques = temp_C.IndexOf("?");
-
-                if (correct_ans.Substring(0, loc_ques).Equals(temp_C.Substring(0, loc_ques)) && correct_ans.Substring(loc_ques + 1).Equals(temp_C.Substring(loc_ques + 1)))
+                string correct_ans = (Int32.Parse(temp[0]) * Int32.Parse(temp[1])).ToString();
+                int loc_ques = temp[2].IndexOf("?");
+                if (correct_ans.Substring(0, loc_ques).Equals(temp[2].Substring(0, loc_ques)) && correct_ans.Substring(loc_ques + 1).Equals(temp[2].Substring(loc_ques + 1)))
                 {
-
                     return (int)Char.GetNumericValue(correct_ans[loc_ques]);
                 }
                 else
                 { return -1; }
-            }
-
-            throw new NotImplementedException();
+            }            
         }
     }
 }
